c831cf3713306932223f99f6203e50990afc0740
zidanhw <zidann.hawari@gmail.com>
2024-08-14 13:36:28 +0700

    error handling with async and mongoose

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const methodOverride = require('method-override')
+const methodOverride = require('method-override');
+const ErrorHandler = require('./ErrorHandler');
 
 // Models
 const Product = require('./models/product');
@@ -20,11 +21,17 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended : true}));
 app.use(methodOverride('_method'));
 
+function wrapAsync(fn) {
+    return function (req, res, next) {
+        fn(req,res,next).catch(err => next(err))
+    }
+}
+
 app.get('/', (req,res) => {
     res.send('hello world!');
 })
 
-app.get('/products', async (req,res) => {
+app.get('/products', wrapAsync(async (req,res) => {
     const { category } = req.query;
     if (category) {
         const products = await Product.find({category});
@@ -35,39 +42,60 @@ app.get('/products', async (req,res) => {
     }
 
     
+}))
+
+app.get('/products/create', (req,res) => {
+    res.render('products/create')
 })
 
-app.get('/products/create', async (req,res) => {
-    res.render('products/create');
-})
-
-app.post('/products', async (req,res) => {
+app.post('/products', wrapAsync(async (req,res) => {
     const product = new Product(req.body);
     await product.save()
     res.redirect(`/products/${product._id}`);
-})
+}))
 
-app.get('/products/:id', async (req,res) => {
+app.get('/products/:id', wrapAsync(async (req,res) => {
     const product = await Product.findById(req.params.id);
     res.render('products/show', { product });
-})
+}))
 
-app.get('/products/:id/edit', async (req,res) => {
+app.get('/products/:id/edit', wrapAsync(async (req,res) => {
     const product = await Product.findById(req.params.id);
-    res.render('products/edit', { product }); 
-})
+    res.render('products/edit', { product });
+}))
 
-app.put('/products/:id', async (req,res) => {
+app.put('/products/:id', wrapAsync(async (req,res) => {
     const { id } = req.params
     const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true});
     res.redirect(`/products/${product._id}`);
+}))
 
-})
-
-app.delete('/products/:id', async (req, res) => {
+app.delete('/products/:id', wrapAsync(async (req, res) => {
     const { id } = req.params;
     await Product.findByIdAndDelete(id);
     res.redirect('/products');
+}))
+
+const validatorHandler = (err) => {
+    err.status = 400
+    err.message = err.message = Object.values(err.errors).map(item => item.message);
+    return new ErrorHandler(err.message,err.status);
+}
+
+app.use((err,req,res,next) => {
+    console.dir(err);
+    if (err.name === 'ValidationError') err = validatorHandler(err);
+    if (err.name === 'CastError') {
+        err.status = 404;
+        err.message = 'Product not found'
+    } 
+
+    next(err);  
+})
+
+app.use((err,req,res,next) => {
+    const { status = 500, message = 'Something went wrong.' } = err;
+    res.status(status).send(message);
 })
 
 
--- models/product.js
+++ models/product.js
@@ -3,19 +3,19 @@ const mongoose = require('mongoose');
 const productSchema = new mongoose.Schema({
     name : { 
         type: String,
-        required: true
+        required: [true, 'Name can not be empty']
     },
     brand : {
         type: String,
-        required: true
+        required: [true, 'Brand can not be empty']
     },
     price : {
         type: Number,
-        required: true
+        required: [true, 'Number can not be empty']
     },
     color : {
         type: String,
-        required: true
+        required: [true, 'Color can not be empty']
     },
     category : {
         type: String,
